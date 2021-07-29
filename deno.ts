11896689f8821f6d0b216c6cc1743e02ed300796
Xin shen <363088847@qq.com>
2021-07-29 12:50:55 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,38 +1,38 @@
-async function handleRequest(request) {
-  const { pathname } = new URL(request.url)
-  if (pathname === "/" || pathname === '/zh') {
-    return index()
-  } else if (pathname[0] === "/") {
-    const data = await fetch(
-      `https://github.com/botshen/gulu-ui-website/${pathname}`
-    )
-      .then((res) => res.text())
-      .then((data) => data)
-    return new Response(data, {
-      status: 200,
-      headers: {
-        server: "denosr",
-        "content-type": pathname.includes(".css")
-          ? "text/css; charset=utf-8"
-          : "text/plain",
-      },
-    })
+async function handleRequest(request: Request) {
+  const { pathname } = new URL(request.url);
+
+  // This is how the proxy works:
+  // 1. A request comes in for a specific asset.
+  // 2. We construct a URL to that asset.
+  // 3. We fetch the asset and respond to the request.
+
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/style.css")) {
+    //  Construct a new URL to style.css by using the URL
+    //  of the script (mod.ts) as base (import.meta.url).
+    const style = new URL("style.css", import.meta.url);
+    // Fetch the asset and return the fetched response
+    // to the client.
+    return fetch(style);
   }
-}
 
-async function index() {
-  const data = await fetch(
-    `https://github.com/botshen/gulu-ui-website/index.html`
-  )
-    .then((res) => res.text())
-    .then((data) => data)
-  return new Response(data, {
-    headers: {
-      "content-type": "text/html; charset=UTF-8",
+  return new Response(
+    `<html>
+      <head>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <h1>Example</h1>
+      </body>
+    </html>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
     },
-  })
+  );
 }
 
-addEventListener("fetch", (event: any) => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request));
+});
