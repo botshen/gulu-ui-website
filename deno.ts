8a0869374d2b8ee942247b83b45ba5f972df6268
Xin shen <363088847@qq.com>
2021-08-28 15:50:08 +0800

    这种

--- deno.ts
+++ deno.ts
@@ -7,14 +7,7 @@ async function handleRequest(request: Request) {
   // 3. We fetch the asset and respond to the request.
 
   // Check if the request is for style.css.
-  if (pathname.startsWith("/style.css")) {
-    //  Construct a new URL to style.css by using the URL
-    //  of the script (mod.ts) as base (import.meta.url).
-    const style = new URL("style.css", import.meta.url);
-    // Fetch the asset and return the fetched response
-    // to the client.
-    return fetch(style);
-  }
+
 
   return new Response(
     fs.readFileSync('./index.html'),
