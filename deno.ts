6068537dfa28f39eb87587c63c7fd8bd706803ef
Xin shen <363088847@qq.com>
2021-08-28 15:57:27 +0800

    999

--- deno.ts
+++ deno.ts
@@ -1,44 +1,38 @@
-function handleRequest(request) {
+async function handleRequest(request: Request) {
   const { pathname } = new URL(request.url);
+  console.log(pathname)
+  // This is how the proxy works:
+  // 1. A request comes in for a specific asset.
+  // 2. We construct a URL to that asset.
+  // 3. We fetch the asset and respond to the request.
 
-  if (pathname.startsWith("/html")) {
-    const html = fs.readFileSync('./index.html');
-
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
-
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/style.css")) {
+    //  Construct a new URL to style.css by using the URL
+    //  of the script (mod.ts) as base (import.meta.url).
+    const style = new URL("style.css", import.meta.url);
+    // Fetch the asset and return the fetched response
+    // to the client.
+    return fetch(style);
   }
 
   return new Response(
-    fs.readFileSync('./index.html'),
+    `<html>
+      <head>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <h1>Example</h1>
+      </body>
+    </html>`,
     {
       headers: {
-        "content-type": "text/html; charset=UTF-8",
+        "content-type": "text/html; charset=utf-8",
       },
     },
   );
 }
 
-addEventListener("fetch", (event) => {
+addEventListener("fetch", (event: FetchEvent) => {
   event.respondWith(handleRequest(event.request));
 });
